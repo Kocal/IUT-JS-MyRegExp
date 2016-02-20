f3e4402b9e474a9891ae78cc66220a48e33218b4
ALLIAUME Hugo <kocal@live.fr>
2016-02-20 18:22:44 +0100

    Optimisation sur les events keyup des zones de texte

--- js/App.js
+++ js/App.js
@@ -2,13 +2,13 @@ function App(myRegExp, $regex, $text, $resultTest, $resultExec) {
 
     var els = [$regex, $text, $resultTest, $resultExec];
 
-    if (!(
-        myRegExp instanceof MyRegExp)) {
+    if (!(myRegExp instanceof MyRegExp)) {
         throw new Error("Expected an instance of `MyRegExp`");
     }
 
     for (var i = 0, len = els.length; i < len; i++) {
         var el = els[i];
+
         if (el == null) {
             throw new Error("One of elements cannot be found");
         }
@@ -27,7 +27,7 @@ App.prototype.init = function () {
 };
 
 App.prototype.update = function () {
-    var regex = this.$regex.value;
+    var regex = this.$regex.value.trim();
     var text = this.$text.value;
 
     var isValid = this.myRegExp.isValid(regex);
@@ -36,9 +36,8 @@ App.prototype.update = function () {
 
     if (!isValid) {
         this.$regex.classList.add('input-error');
-
         this.$resultTest.textContent =
-        this.$resultExec.textContent = "Regex invalide";
+            this.$resultExec.textContent = "Invalid regex";
     } else {
         this.$regex.classList.remove('input-error');
         this.$resultTest.textContent = resultTest;
@@ -50,11 +49,26 @@ App.prototype.update = function () {
 App.prototype.bindEvents = function () {
     var self = this;
 
+    // On évitera de lancer le parsing de la regex si on ne fait que déplacer
+    // le curseur dans les zones de text
+    var regexValue = this.$regex.value;
+    var textValue = this.$text.value;
+
     this.$regex.addEventListener('keyup', function (e) {
+        if (regexValue == this.value) {
+            return;
+        }
+
+        regexValue = this.value;
         return self.onRegexChange(e);
     });
 
     this.$text.addEventListener('keyup', function (e) {
+        if (textValue == this.value) {
+            return;
+        }
+
+        textValue = this.value;
         return self.onTextChange(e);
     });
 };
